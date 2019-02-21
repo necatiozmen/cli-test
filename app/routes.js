5569308073ca3ff21350de2320dfe877114b513d
necati <necati@pankod.com>
2019-02-21 17:51:41 +0300

    remove routes

--- app/routes.js
+++ app/routes.js
@@ -4,8 +4,4 @@ routes
 	.add('/detail', '/detail/index')
 	.add('/car/payment', '/order/index')
 
-    .add('/tdfgd', '/Sdsa/index')
-
-    .add('/fdfv', '/Sadasdsa/index')
-
 module.exports = routes;
